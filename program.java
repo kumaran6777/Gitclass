e56cfd91de296e569a43a2db47ec3e50f7049dfa
kumaran6777 <kumaran006777@gmail.com>
2024-02-24 18:54:29 +0530

    welcome

---
+++ program.java
@@ -0,0 +1,11 @@
+package org.in;
+
+public class program {
+private void Bca() {
+	System.out.println("best");
+}
+public static void main(String[] args) {
+	program p = new program();
+	p.Bca();
+}
+}
